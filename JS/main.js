f61085a70c6996cddf5a4dd7d1375b96bfcacb0d
punyapatsuk-cm <punyapat.suk@ku.th>
2026-01-06 11:29:13 +0700

    js2

--- JS/main.js
+++ JS/main.js
@@ -93,12 +93,193 @@
 //     console.log("Odd");
 // }
 
-let conter = 0
-while (conter <=4){
-    conter += 1;
-    console.log("while:", conter); 
-}
+// let conter = 0
+// while (conter <=4){
+//     conter += 1;
+//     console.log("while:", conter); 
+// }
 
-for (let i = 0; i <=4; i++  ){
-    console.log("for:", i);
-}
+// for (let i = 0; i <=4; i++  ){
+//     console.log("for:", i);
+// }
+
+
+///////////////////////////////////
+
+
+// let age1 = 25
+// let age2 = 30
+// let age3 = 35
+// console.log(age1, age2, age3);
+
+// let ages = [25, 30, 35]
+// console.log(ages); // [25,30,35]
+// console.log(ages[1]); // index ที่ 1 = 30
+
+// // แทนที่ค่า array
+// ages = [40, 45, 50]
+// console.log(ages); // [40,45,50]
+
+// // ต่อ,เพื่ม array
+// ages.push(55)
+// console.log(ages); // [40,45,50,55]
+
+// // ความยาวของ array
+// console.log(ages.length); // 4
+
+// // ตัดตัวสุดท้ายออก
+// ages.pop()
+// console.log(ages); // [40,45,50]
+
+// // array ในเงื่อนไข
+// if (ages.includes(45)){ // true
+//     console.log("พย 45 ในอาเรย์");
+// }
+
+// // เรียงลำดับ array จากน้อยไปมาก
+// let numbers = [90, 60, 80, 40, 50]
+// numbers.sort() // เรียงจากน้อยไปมาก
+// console.log("เรียงจากน้อยไปมาก:", numbers); // [40,50,60,80,90]
+
+// // array ใน string
+// let names = ["John", "Alice", "Bob"]
+// names.pop("Alice")
+// names.push("Eve")
+// console.log(names)
+// console.log(names.length)
+// console.log(names[2])
+
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i])
+// }
+
+
+// object //
+
+// let student = [{
+//     name: "John",
+//     age: 20,
+//     grade: "A"
+// },{
+//     name: "Alice",
+//     age: 25,
+//     grade: "B"
+// }]
+
+// for (let i = 0; i < student.length; i++){
+//     console.log("Student", i+1 + ":")
+//     console.log("Name:", student[i].name)
+//     console.log("Age:", student[i].age)
+//     console.log("Grade:", student[i].grade)
+// }
+
+// student.push({
+//     name: "Bob",
+//     age: 22,
+//     grade: "C"
+// })  
+
+// console.log(student)
+
+
+// Function //
+// ประกาศฟังก์ชัน
+
+// function calculate_grade(score){
+//     if (score >= 80) {
+//         grade = "A";
+//     }  else if (score >= 70 ){
+//         grade = "B";      
+//     }  else if (score >= 60 ){
+//         grade = "C";      
+//     }  else if (score >= 50 ){
+//         grade = "D";
+//     }  else {
+//         grade = "F";    
+//     }
+//     return grade;
+// }
+
+// let student_score = 75
+// let student_geade = calculate_grade(student_score)
+// console.log(student_geade)
+
+
+// array //
+
+// let score = [10, 20, 30, 40, 50]
+
+// for (let i = 0; i < score.length; i++) {
+//     // console.log(score[i])
+//     console.log(`Score at index ${i} is ${score[i]}`)
+// }
+
+// score.forEach((s) => {
+//     console.log('score', s)
+// })
+
+
+// score = score.map((s) => {
+//     return s * 2
+// })
+
+// score.forEach((s) => {
+//     console.log('new score:', s)
+// })
+
+// let score = [10, 20, 30, 40, 50]
+
+
+// for (let index =  0; index < score.length; index++) {
+//     console.log('score', score[index])
+
+// }
+// let newScore = score.filter((ns) => {
+//     return ns >= 30
+    
+// })
+
+
+// newScore.forEach((s) => {
+//     console.log('new score :',s)
+// })
+
+
+// array + function //
+
+let students = [
+    {
+        name: 'aa',
+        score: '50',
+        grade: 'D'
+    },
+    {
+        name: 'bb',
+        score: '120',
+        grade: 'A'
+    }
+]
+
+console.log('student : ', students[0])
+
+let student = students.find((s) => {
+    if (s.name == 'bb'){
+        return true
+    }
+})
+
+let doublescore_student = students.map((s) => {
+    s.score = s.score * 2
+    return s 
+})
+
+console.log('student : ', student)
+console.log(doublescore_student)
+
+let highScore_student = students.filter((s) => {
+    if (s.score >= 110){
+        return true
+    }
+})
+
+console.log('high score student : ', highScore_student)
